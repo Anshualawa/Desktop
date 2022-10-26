279b01018a8dff874d783d10cdabe6a67368fff3
Pappu Alawa <95533048+Anshualawa@users.noreply.github.com>
2022-10-26 13:38:10 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,101 @@
+
+//<-------------start App Store JavaScript---------------->
+function AppStore(){
+    window.open("Appstore.html","_self");
+}
+function Python(){
+    window.open("https://www.javatpoint.com/python-tutorial")
+}
+function JavaScript(){
+    window.open("https://www.javatpoint.com/javascript-tutorial")
+}
+function Java(){
+    window.open("https://www.javatpoint.com/java-tutorial")
+}
+//<-------------End App Store JavaScript---------------->
+
+
+function Photo(){
+    window.open("phots.html","_self");
+}
+
+function About(){
+    window.open("about.html","_self");
+    
+}
+
+function Testing(){
+    window.open("Testing.html", "_self");
+}
+
+function Terminal(){
+    window.open("terminal.html","_self");
+}
+
+function Whatsapp(){
+    window.open("https://web.whatsapp.com/","_self");
+}
+
+function Instagram(){
+    window.open("https://www.instagram.com/","_self");
+}
+
+function Twitter(){
+    window.open("https://twitter.com/home","_self");
+}
+
+function YouTube(){
+    window.open("https://www.youtube.com/","_self");
+}
+
+function MP3(){
+    window.open("Music_mp3.html","_self");
+}
+
+function Play(){
+    document.getElementById("")="typing-6458.mp3";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let hackerBtn = document.querySelector('button');
+hackerBtn.addEventListener('click',inputMsg);
+
+
+function inputMsg(){
+    let name = prompt("Enter Name of Testing Engineer");
+    hackerBtn.textContent = 'Review Engineer:'+name;
+}
+
+
+
+
+
